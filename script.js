5861d7f2cf15d719ed439d198ab90cc86306cd3e
Shikabek <raiymbek.kambarov@gmail.com>
2023-01-10 13:27:20 +0600

    updated 10.01.23

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
+let computerSelection = getComputerChoise()
+let x = prompt("Enter your variant")
+let playerSelection = x[0].toUpperCase() + x.slice(1).toLowerCase()
+
 function getComputerChoise (){
     let chance = Math.floor(Math.random() * 3);
     if (chance == 0) {
-        return 'rock';
+        return 'Rock';
     }else if (chance == 1) {
-        return 'paper';
+        return 'Paper';
     }else {
-        return 'scissors'
+        return 'Scissors'
     }
 }
 
-let computerSelection = getComputerChoise()
-let x = prompt("Enter your variant")
-let playerSelection = x.slice(0).toLowerCase()
-
-function singleRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'Player wins this round!';
-    }else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'Computer wins this round!';
-    }else if (playerSelection === 'rock' && computerSelection === 'rock') {
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
         return 'It`s a tie!'
-    }else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'Player wins this round!';
-    }else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'Computer wins this round!';
-    }else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-        return 'It`s a tie!'
-    }else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'Player wins this round!';
-    }else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'Computer wins this round!';
-    }else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        return 'It`s a tie!'
+    }else if (
+        playerSelection == 'Rock' && computerSelection == 'Scissors' ||
+        playerSelection == 'Paper' && computerSelection == 'Rock' ||
+        playerSelection == 'Scissors' && computerSelection == 'Paper' 
+    ) {
+        return 'User wins round'
+    }else if(
+        computerSelection == 'Rock' && playerSelection == 'Scissors' ||
+        computerSelection == 'Paper' && playerSelection == 'Rock' ||
+        computerSelection == 'Scissors' && playerSelection == 'Paper'
+    ){
+        return 'Computer wins round'
     }
 }
 
+console.log(playRound(playerSelection,computerSelection))
+console.log(computerSelection)
