2b606aaea44dd4a40b0c95fa35d3f3fee7518637
Shikabek <raiymbek.kambarov@gmail.com>
2023-01-11 12:26:16 +0600

    .\script.js
    updated script 11.01.23

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
-let computerSelection = getComputerChoise()
-let x = prompt("Enter your variant")
-let playerSelection = x[0].toUpperCase() + x.slice(1).toLowerCase()
-
-function getComputerChoise (){
-    let chance = Math.floor(Math.random() * 3);
-    if (chance == 0) {
-        return 'Rock';
-    }else if (chance == 1) {
-        return 'Paper';
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3)
+    if (choice == 0) {
+        return 'Rock'
+    }else if (choice == 1){
+        return 'Paper'
     }else {
         return 'Scissors'
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function checkWinner(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return 'It`s a tie!'
+        return 'Tie!'
     }else if (
         playerSelection == 'Rock' && computerSelection == 'Scissors' ||
         playerSelection == 'Paper' && computerSelection == 'Rock' ||
         playerSelection == 'Scissors' && computerSelection == 'Paper' 
     ) {
-        return 'User wins round'
-    }else if(
-        computerSelection == 'Rock' && playerSelection == 'Scissors' ||
-        computerSelection == 'Paper' && playerSelection == 'Rock' ||
-        computerSelection == 'Scissors' && playerSelection == 'Paper'
-    ){
-        return 'Computer wins round'
+        return 'Player'
+    }else {
+        return 'Computer'
     }
 }
 
-console.log(playRound(playerSelection,computerSelection))
-console.log(computerSelection)
+function playRound(playerSelection, computerSelection) {
+    const winner = checkWinner(playerSelection, computerSelection)
+    if(winner == "Tie!"){
+        return "It's a Tie!"
+    }
+    else if(winner == "Player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+function game() {
+    let scorePlayer = 0
+    let scoreComputer = 0
+    console.log('Welcome')
+    for(let i=0;i<5;i++) {
+        let x = prompt("Enter your variant")
+        let playerSelection = x[0].toUpperCase() + x.slice(1).toLowerCase()
+        const computerSelection = getComputerChoice()
+        console.log(playRound(playerSelection,computerSelection))
+        if(checkWinner(playerSelection, computerSelection) == 'Player') {
+            scorePlayer++
+        }else if (checkWinner(playerSelection, computerSelection) == 'Computer') {
+            scoreComputer++
+        }
+    }
+    console.log('Game Over')
+    if(scorePlayer > scoreComputer) {
+        console.log('User won the game')
+        console.log(`Final score: user - ${scorePlayer} vs. comp - ${scoreComputer}`)
+    }else if (scorePlayer < scoreComputer) {
+        console.log('Comp won the game')
+        console.log(`Final score: comp - ${scoreComputer} vs. user - ${scorePlayer}`)
+    }else {
+        console.log('A tie!')
+        console.log(`Final score: user - ${scorePlayer} vs. comp - ${scoreComputer}`)
+    }
+}
+game()  
